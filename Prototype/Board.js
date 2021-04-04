e8dceac16c935160cce2120ca703b1be14212f7f
Emma Johnson <emma.ro.johnson@gmail.com>
2021-04-03 19:23:49 -0700

    Setting background

--- Prototype/Board.js
+++ Prototype/Board.js
@@ -5,6 +5,8 @@ function createImage(){
   
     g = document.createElement('div'); 
     g.setAttribute("class", "box makeMeDraggable ui-draggable");
+    g.setAttribute("style", "top: 0px;");
+
     $('.makeMeDraggable').draggable(); 
     document.getElementById("board").append(g);
     init();
@@ -33,6 +35,32 @@ function clear(){
     document.querySelectorAll('.box').forEach(e => e.remove());
 }
 
-function changeBackground(){
-    
+function background(num){
+    $("#board").removeClass();
+    var back = document.getElementById("board");
+
+    if(num == 0){
+       
+    }else if(num == 1){
+        back.classList.add("grid");
+    }else if(num == 2){
+        back.classList.add("col3");
+    }else if(num == 3){
+        back.classList.add("col4");
+    }else if(num == 4){
+        back.classList.add("square");
+    }else if(num == 5){
+
+    }else{
+
+    }
 }
+
+//get url of uploaded background
+$(document).ready(function(){
+    document.getElementById('picback').onchange = function (evt) {
+        if (this.files && this.files[0]) {
+            document.getElementById("backgroundImage").src = URL.createObjectURL(this.files[0]); // set src to blob url
+        }
+    }
+    });
